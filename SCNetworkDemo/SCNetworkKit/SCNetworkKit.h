3c3b9fac241bc24980a333ba95b86d9c2ec7f455
qianlongxu <qianlongxu@sohu-inc.com>
2017-07-20 16:48:29 +0800

    clean JSONUtil import

--- SCNetworkDemo/SCNetworkKit/SCNetworkKit.h
+++ SCNetworkDemo/SCNetworkKit/SCNetworkKit.h
@@ -11,7 +11,6 @@
 #import "NSDictionary+SCAddtions.h"
 #import "NSString+SCAddtions.h"
 #import "NSObject+SCCancelRef.h"
-#import "SCJSONUtil.h"
 
 #import "SCNetworkService.h"
 #import "SCNetworkRequest.h"
