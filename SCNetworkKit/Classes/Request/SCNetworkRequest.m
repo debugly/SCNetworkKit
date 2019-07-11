e5cace4cfbcad8b31c451c1a2a80ee9ffc067446
qianlongxu <qianlongxu@sohu-inc.com>
2019-07-11 11:01:13 +0800

    解决macOS 默认UA不同机器出现中文导致不全问题；

--- SCNetworkKit/Classes/Request/SCNetworkRequest.m
+++ SCNetworkKit/Classes/Request/SCNetworkRequest.m
@@ -15,6 +15,7 @@
 #import <UIKit/UIDevice.h>
 #import <UIKit/UIScreen.h>
 #endif
+#import <sys/sysctl.h>
 #import "SCNHTTPBodyStream.h"
 #import "SCNHeader.h"
 
@@ -35,13 +36,37 @@ static dispatch_queue_t SCN_Response_Parser_Queue() {
 /*
  SCNetworkRequest默认UA格式如下:
  %E6%90%9C%E7%8B%90%E8%A7%86%E9%A2%91/1 SCNDemo/1.0.8 (iPhone; iOS 11.4; Scale/2.00)
- %E6%90%9C%E7%8B%90%E5%BD%B1%E9%9F%B3/1 SCNMacDemo/1.0.8 (Macintosh; Mac OS X Version 10.14.1 (Build 18B75))
+ %E6%90%9C%E7%8B%90%E5%BD%B1%E9%9F%B3/1 SCNMacDemo/1.0.8 (MacBookPro11,5; Mac OS X 10.14.5)
  https://stackoverflow.com/questions/36379347/does-nsurlsession-send-user-agent-automatically
  
  不指定时，系统默认的 UA格式如下:
  SCNDemo/1 CFNetwork/901.1 Darwin/18.2.0
  %E6%90%9C%E7%8B%90%E5%BD%B1%E9%9F%B3/17141 CFNetwork/975.0.3 Darwin/18.2.0 (x86_64)
  */
+#if TARGET_OS_OSX
++ (NSString *)deviceModel {
+    NSString *model = nil;
+    
+    int error = 0;
+    int value = 0;
+    size_t length = sizeof(value);
+    
+    error = sysctlbyname("hw.model", NULL, &length, NULL, 0);
+    if (error == 0) {
+        char *cpuModel = (char *)malloc(sizeof(char) * length);
+        if (cpuModel != NULL) {
+            error = sysctlbyname("hw.model", cpuModel, &length, NULL, 0);
+            if (error == 0) {
+                model = @(cpuModel);
+            }
+            free(cpuModel);
+        }
+    }
+    
+    return model;
+}
+#endif
+
 + (NSString *) defaultUA
 {
     static NSString *ua;
@@ -68,11 +93,13 @@ static dispatch_queue_t SCN_Response_Parser_Queue() {
             shortVersion = buildNum;
         }
         
-        
 #if TARGET_OS_IPHONE
         ua = [NSString stringWithFormat:@"%@/%@ %@/%@ (%@; iOS %@; Scale/%0.2f)",displayName, buildNum, bundleName, shortVersion, [[UIDevice currentDevice] model], [[UIDevice currentDevice] systemVersion], [[UIScreen mainScreen] scale]];
 #elif defined(__MAC_OS_X_VERSION_MIN_REQUIRED)
-        ua = [NSString stringWithFormat:@"%@/%@ %@/%@ (Macintosh; Mac OS X %@)", displayName, buildNum, bundleName, shortVersion, [[NSProcessInfo processInfo] operatingSystemVersionString]];
+        NSOperatingSystemVersion sysVersion = [[NSProcessInfo processInfo] operatingSystemVersion];
+        NSString *osv = [NSString stringWithFormat:@"%ld.%ld.%ld",sysVersion.majorVersion,sysVersion.minorVersion,sysVersion.patchVersion];
+        
+        ua = [NSString stringWithFormat:@"%@/%@ %@/%@ (%@; Mac OS X %@)", displayName, buildNum, bundleName, shortVersion, [self deviceModel],osv];
 #endif
     });
     return ua;
