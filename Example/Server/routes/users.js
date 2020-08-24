d21120a8f3a77967196bfa9f44d72303ab37957f
qianlongxu <qianlongxu@sohu-inc.com>
2020-08-24 15:05:30 +0800

    api resp add method etc.

--- Example/Server/routes/users.js
+++ Example/Server/routes/users.js
@@ -3,19 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var result = {};
-    result['status'] = 200;
-    res.write(JSON.stringify(result));
-    res.end();
+  var result = {};
+  result['status'] = 200;
+  result['method'] = 'GET';
+  result['query'] = req.query;
+  res.write(JSON.stringify(result));
+  res.end();
 });
 
 router.post('/', function(req, res, next) {
   var result = {};
-    result['status'] = 200;
-    result['msg'] = req.body;
-    console.log(req.body);
-    res.write(JSON.stringify(result));
-    res.end();
+  result['status'] = 200;
+  result['method'] = 'POST';
+  result['body'] = req.body;
+  result['query'] = req.query;
+  // console.log(req.body);
+  res.write(JSON.stringify(result));
+  res.end();
 });
 
 module.exports = router;
