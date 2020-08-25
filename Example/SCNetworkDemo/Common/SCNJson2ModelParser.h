176a60e79b82ff35877cb94582328040fc069827
qianlongxu <qianlongxu@sohu-inc.com>
2020-08-25 10:02:50 +0800

    add missing files

---
+++ Example/SCNetworkDemo/Common/SCNJson2ModelParser.h
@@ -0,0 +1,15 @@
+//
+//  SCNJson2ModelParser.h
+//  SohuCoreFoundation
+//
+//  Created by 许乾隆 on 2017/5/19.
+//  Copyright © 2017年 sohu-inc. All rights reserved.
+//
+// 实现了 SCNetworkKit 里的 JSON 转 Model 解析协议！
+
+#import <Foundation/Foundation.h>
+#import "SCNModelParserProtocol.h"
+
+@interface SCNJson2ModelParser : NSObject<SCNModelParserProtocol>
+
+@end
