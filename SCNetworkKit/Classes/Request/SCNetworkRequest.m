7972f4aceccc76ec62fa0da9477144801c0f9b5e
qianlongxu <qianlongxu@gmail.com>
2021-09-01 18:38:11 +0800

    fix bug an empty query paramter get request appends ? or &
    (cherry picked from commit 2ec7d4e40758aeab4a73db38089cbe2746431876)

--- SCNetworkKit/Classes/Request/SCNetworkRequest.m
+++ SCNetworkKit/Classes/Request/SCNetworkRequest.m
@@ -274,17 +274,18 @@ static dispatch_queue_t SCN_Response_Parser_Queue() {
 {
     NSAssert(urlString, @"makeURLRequest:url不能为空");
     NSString *queryStr = [SCNUtil makeUrlEncodeingString:parameters];
-    
-    if (NSNotFound != [urlString rangeOfString:@"?"].location) {
-        NSString *join = @"&";
-        if ([urlString hasSuffix:@"&"]) {
-            join = @"";
+    if (queryStr.length > 0) {
+        if (NSNotFound != [urlString rangeOfString:@"?"].location) {
+            NSString *join = @"&";
+            if ([urlString hasSuffix:@"&"]) {
+                join = @"";
+            }
+            urlString = [NSString stringWithFormat:@"%@%@%@", urlString,join,queryStr];
+        } else {
+            urlString = [NSString stringWithFormat:@"%@?%@", urlString,queryStr];
         }
-        urlString = [NSString stringWithFormat:@"%@%@%@", urlString,join,queryStr];
-    } else {
-        urlString = [NSString stringWithFormat:@"%@?%@", urlString,queryStr];
     }
-    
+
     NSURL *url = [NSURL URLWithString:urlString];
     
     NSAssert(url, @"makeURLRequest:url不合法");
@@ -555,9 +556,11 @@ static dispatch_queue_t SCN_Response_Parser_Queue() {
              [NSString stringWithFormat:@"application/x-www-form-urlencoded; charset=%@", charset]
                   forHTTPHeaderField:@"Content-Type"];
             NSString *bodyStr = [SCNUtil makeUrlEncodeingString:self.parameters];
-            NSData *body = [bodyStr dataUsingEncoding:NSUTF8StringEncoding];
-            if (body) {
-                [createdRequest setHTTPBody:body];
+            if ([bodyStr length] > 0) {
+                NSData *body = [bodyStr dataUsingEncoding:NSUTF8StringEncoding];
+                if (body) {
+                    [createdRequest setHTTPBody:body];
+                }
             }
         }
             break;
