99a9d0cc07e1059af92fba11652abf5bf6b36716
Youshi Qian <youshiqian206918@sohu-inc.com>
2023-02-13 17:47:18 +0800

    resume download task only when the task's state is correct

--- SCNetworkKit/Classes/Request/SCNetworkRequest+SessionDelegate.m
+++ SCNetworkKit/Classes/Request/SCNetworkRequest+SessionDelegate.m
@@ -119,7 +119,9 @@ totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
             if (speed > downloadRequest.speedLimit) {
                 [downloadTask suspend];
                 dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                    [downloadTask resume];
+                    if (self.state == SCNKRequestStateStarted) {
+                        [downloadTask resume];
+                    }
                 });
             }
         }
